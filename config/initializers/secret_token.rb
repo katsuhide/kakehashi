1ffd460996b186fb527390aa94b3186445a91fe5
katsuhide <kamonegi.gg@gmail.com>
2014-03-20 08:40:20 +0900

    import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kakehashi::Application.config.secret_key_base = '79c9815518327b700d50285a26281c9ff0e2a4befe018f06a50d59eb5944a7476b3e04bdc910d06a4d350ebfde45878c432a4c65db7a41e54780cf45b1c138ef'
