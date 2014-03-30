24f74eddd39218af48ea7dc81cd11e96b8aa613b
katsuhide <kamonegi.gg@gmail.com>
2014-03-31 02:21:47 +0900

    生産地を表示

--- app/controllers/day_controller.rb
+++ app/controllers/day_controller.rb
@@ -3,7 +3,8 @@ class DayController < ApplicationController
 	respond_to :json
 
 	def list
-		@days = Day.all
+		# @days = Day.all
+		@days = Day.joins("LEFT JOIN keywords ON keywords.tag = days.tag").select('*')
 		respond_with @days
 	end
 
